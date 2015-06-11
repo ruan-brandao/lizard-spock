91eb0d3380e3fb4ae45fa39888c06593dfa99bf0
Ruan Brandão <ruan.bernardo@gmail.com>
2015-06-11 01:55:02 -0300

    Implement Game#choose_shape

--- lib/game.rb
+++ lib/game.rb
@@ -1,4 +1,7 @@
 class Game
+  SHAPES = [:rock, :paper, :scissors, :lizard, :spock]
+  attr_reader :shape
+
   def initialize(output = STDOUT)
     @output = output
   end
@@ -6,5 +9,11 @@ class Game
   def start
     initial_message = "Welcome to the Rock, Paper, Scissors, Lizard, Spock Game!"
     @output.puts initial_message
+    choose_shape
+  end
+
+  def choose_shape
+    index = Kernel.rand(5)
+    @shape = SHAPES[index]
   end
 end
--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -2,7 +2,7 @@ require "spec_helper"
 require "game"
 
 describe Game do
-  let(:output) { double("output") }
+  let(:output) { double("output").as_null_object }
 
   subject(:game) { Game.new(output) }
 
@@ -12,6 +12,36 @@ describe Game do
       expect(output).to receive(:puts).with(initial_message)
 
       game.start
+    end
+
+    it "chooses a hand shape" do
+      expect(game).to receive(:choose_shape)
+
+      game.start
+    end
+  end
+
+  describe "#choose_shape" do
+    it "sets the shape for game" do
+      game.choose_shape
+
+      expect(game.shape).to_not be_nil
+    end
+
+    it "chooses a valid shape" do
+      valid_shapes = Game::SHAPES
+
+      game.choose_shape
+
+      expect(valid_shapes).to include(game.shape)
+    end
+
+    it "chooses the shape randomly" do
+      allow(Kernel).to receive(:rand).and_return(3)
+
+      game.choose_shape
+
+      expect(game.shape).to eq(Game::SHAPES[3])
     end
   end
 end
